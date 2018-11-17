4869619eceb41f497092dedbcc7d625edc39529b
Suresh <sureshgopineni@gmail.com>
2018-11-17 02:20:18 -0500

    #I2 ProductsController Adding

---
+++ MVCApplication/Controllers/Farmer_CropController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class Farmer_CropController : Controller
+    {
+        // GET: Farmer_Crop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
